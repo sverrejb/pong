0f2ec11b50c4df3774ebed6355fea6fd3d4aab01
Sverre Johann Bjørke <sverrejb@gmail.com>
2019-03-01 20:35:53 +0100

    removed unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use amethyst::prelude::*;
-use amethyst::renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline,
-    RenderBundle, Stage, VirtualKeyCode};
+use amethyst::renderer::{DisplayConfig, DrawFlat2D, Pipeline,
+    RenderBundle, Stage};
 
-use amethyst::ui::{DrawUi, UiBundle};
+use amethyst::ui::{DrawUi};
 use amethyst::core::transform::TransformBundle;
 use amethyst::input::InputBundle;
 
